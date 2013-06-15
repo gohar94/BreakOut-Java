a69eb922cc38c06cce65b3eb8e6bf10a60d24381
S <s>
2013-06-16 04:53:20 +0500

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -150,12 +150,6 @@ public class Breakout extends GraphicsProgram {
 		ball.setFillColor(Color.BLACK);
 		ball.setFilled(true);
 		
-		vx = rgen.nextDouble(1.0, 3.0);
-		if (rgen.nextBoolean(0.5)) vx = -vx;
-		System.out.println(vx);
-		ball.move(vx, vy);
-		pause(1);
-		
 	}
 	
 /* Method: run() */
@@ -175,6 +169,13 @@ public class Breakout extends GraphicsProgram {
 		addMouseListeners();
 		ball();
 		
+//		vx = rgen.nextDouble(1.0, 3.0);
+//		if (rgen.nextBoolean(0.5)) vx = -vx;
+		System.out.println(vx);
+		while(!done){
+		ball.move(vx, vy);
+		pause(1);
+		}
 		/* You fill this in, along with any subsidiary methods */
 	}
 
