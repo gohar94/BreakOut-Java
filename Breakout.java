0ab065a7dcadde67d816a2d3f46f992b70efc65e
S <s>
2013-06-16 15:03:16 +0500

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -67,7 +67,7 @@ public class Breakout extends GraphicsProgram {
 	int screenres_y=750;
 	
 	int paddlex=((screenres_x-APPLICATION_WIDTH)/2)+(APPLICATION_WIDTH/2);
-	int paddley= (((screenres_y-APPLICATION_HEIGHT)/2)+APPLICATION_HEIGHT-PADDLE_Y_OFFSET);
+	int paddley= (((screenres_y-APPLICATION_HEIGHT)/2)+APPLICATION_HEIGHT-PADDLE_Y_OFFSET-PADDLE_HEIGHT);
 	GRect paddle = new GRect(paddlex, paddley, PADDLE_WIDTH, PADDLE_HEIGHT);
 	
 	int lowerbound= (screenres_x - APPLICATION_WIDTH)/2;
